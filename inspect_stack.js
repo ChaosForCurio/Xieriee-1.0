0fe22b47becfa377f41bb230b44eb622fbbe48af
Ash Nayak <bornash0001@gmail.com>
2025-11-20 09:57:31 +0530

    feat: Add scripts to inspect StackServerApp and temporary type definition files.

---
+++ inspect_stack.js
@@ -0,0 +1,10 @@
+const { StackServerApp } = require('@stackframe/stack');
+
+try {
+    console.log('StackServerApp prototype keys:', Object.getOwnPropertyNames(StackServerApp.prototype));
+    const app = new StackServerApp({ tokenStore: 'nextjs-cookie' });
+    console.log('StackServerApp instance keys:', Object.keys(app));
+    console.log('StackServerApp instance prototype keys:', Object.getOwnPropertyNames(Object.getPrototypeOf(app)));
+} catch (e) {
+    console.error(e);
+}
